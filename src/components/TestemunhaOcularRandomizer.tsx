790aa99ba9f16599a212736a4ea3730756051608
kavispires <kavispires@gmail.com>
2024-02-25 22:41:21 -0800

    Added testemunha ocular randomizer

--- src/components/TestemunhaOcularRandomizer.tsx
+++ src/components/TestemunhaOcularRandomizer.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Content } from './Content';
 import { random } from 'lodash';
 import clsx from 'clsx';
-import { Button } from 'antd';
+import { Button, Space, Typography } from 'antd';
 
 const suspects = new Array(12).fill(0).map((_, i) => i + 1);
 
@@ -15,16 +15,19 @@ export function TestemunhaOcularRandomizer() {
 
   return (
     <Content centered>
-      <Button type="primary" size="large" onClick={onRandomize}>
-        Sorteie um suspeito
-      </Button>
-      <div className="suspects">
-        {suspects.map((suspect) => (
-          <div key={suspect} className={clsx('suspect', value === suspect && 'suspect--target')}>
-            {suspect}
-          </div>
-        ))}
-      </div>
+      <Space direction="vertical" className="content content--center">
+        <Typography.Title level={2}>Testemunha Ocular</Typography.Title>
+        <Button type="primary" size="large" onClick={onRandomize}>
+          Sorteie um suspeito
+        </Button>
+        <div className="suspects">
+          {suspects.map((suspect) => (
+            <div key={suspect} className={clsx('suspect', value === suspect && 'suspect--target')}>
+              {suspect}
+            </div>
+          ))}
+        </div>
+      </Space>
     </Content>
   );
 }
